bcb3e4d829faa742d35533c45d0e06e6dcb29f40
Oidenhiops <juanzateorozco@gmail.com>
2025-11-15 01:17:45 -0500

    feat: Update dialog system with new button interactions and add performance test run settings

--- Assets/Scripts/Dialog/DialogBanner/DialogBanner.cs
+++ Assets/Scripts/Dialog/DialogBanner/DialogBanner.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DialogBanner : MonoBehaviour, ISubmitHandler
+public class DialogBanner : MonoBehaviour, IPointerClickHandler, ISubmitHandler
 {
     public DialogBaseSO newDialog;
     public DialogManager dialogManager;
     public ManagementLanguage managementLanguage;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!dialogManager.isResettingDialog) _ = dialogManager.OnHandleSelectBanner(this);
+    }
+
     public void OnSubmit(BaseEventData eventData)
     {
         if (!dialogManager.isResettingDialog) _ = dialogManager.OnHandleSelectBanner(this);
